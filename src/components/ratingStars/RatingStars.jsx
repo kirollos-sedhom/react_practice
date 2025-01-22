5d80d9c94093263058e47114fdeeac849c1344cb
carlo <kirollosedhom@gmail.com>
2025-01-22 17:09:04 +0200

    finish star component

--- src/components/ratingStars/RatingStars.jsx
+++ src/components/ratingStars/RatingStars.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
-
-const RatingStars = () => {
+import { FaStar } from "react-icons/fa";
+function RatingStars(){
+  const [selected, setSelected] = React.useState(-1)
+  const [hovered, setHovered] = React.useState(-1)
   return (
     <div className='flex justify-center mt-4'>
+      {[...Array(6)].map(
+        (curr,index)=>{
+          
+          return (
+            <FaStar 
+            onClick={() =>setSelected(index)}
+            onMouseEnter={()=>setHovered(index)}
+            onMouseLeave={()=>setHovered(selected)}
+            key={index}
+            className={`${index <= hovered ? "fill-yellow-500" : ""}`}
+            />
+          )
+        }
+      )}
       
-    
     </div>
   )
 }
