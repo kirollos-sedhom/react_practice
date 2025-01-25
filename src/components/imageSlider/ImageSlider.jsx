4af6b48dbf0cfadbe2132546b8fa3122758f254c
carlo <kirollosedhom@gmail.com>
2025-01-25 19:11:43 +0200

    image slider finished

--- src/components/imageSlider/ImageSlider.jsx
+++ src/components/imageSlider/ImageSlider.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { IoIosArrowDroprightCircle,IoIosArrowDropleftCircle  } from "react-icons/io";
 
 function ImageSlider() {
     const [data, setData] = React.useState(null)
     const [isLoading, setIsLoading] = React.useState(false)
     const [error, setError] = React.useState(null)
-    const [images,setImages] = React.useState([])
+
     const [currentImage, setCurrentImage] = React.useState(0)
 
 async function getData(){
@@ -18,9 +19,7 @@ async function getData(){
         }
         const json = await response.json()
         setData(json)
-        let newImageLinks = []
-        json.forEach(item=>newImageLinks.push(<img src={item.urls.full} />))
-        setImages(newImageLinks)
+
         
     }
     catch(error){
@@ -36,7 +35,7 @@ React.useEffect(()=>{
     getData()
 
 },[])
-// console.log(images)
+console.log(data)
 if (isLoading) {
     return (<h1>loading</h1>)
 }else{
@@ -52,8 +51,35 @@ if (isLoading) {
 
       {/* Only render the actual data if it's available */}
       {data && 
-       images[currentImage] }
-        <button onClick={()=>setCurrentImage(currentImage+1)}>next</button>
+      (
+        // images container
+        <div className='relative w-96 h-96 border-2'>
+        <IoIosArrowDropleftCircle className='z-10 fill-white size-8 absolute bottom-1/2 left-0' onClick={()=> currentImage===0? setCurrentImage(data.length-1) : setCurrentImage(currentImage-1)} />
+            {
+                data.map((image,index)=>{
+                    const classList = index===currentImage? "h-full w-full object-contain transition-opacity duration-500" : "hidden"
+                    return (
+                        <>
+                        <img key={index} alt={image.alt_description} src={image.urls.full} className={`absolute w-full h-full object-cover transition-opacity duration-500 ${
+          index === currentImage ? "opacity-100" : "opacity-0"
+        }`}/>
+                        
+                        </>
+            
+                )
+                  })
+            }
+            <IoIosArrowDroprightCircle className='fill-white size-8 absolute bottom-1/2 right-0' onClick={()=> currentImage===data.length-1? setCurrentImage(0) : setCurrentImage(currentImage+1)} /> 
+            <div className='buttons absolute bottom-10 right-1/2'>
+                {[...Array(data.length)].map((item,index)=>{
+                    return <button key={index} onClick={()=>setCurrentImage(index)} className={currentImage===index? 'bg-gray-700 rounded-full p-2':"disabled bg-gray-200  rounded-lg rounded-full p-2"}></button>
+                })}
+            </div>
+            
+        </div>
+      )}
+        {/* <button onClick={()=>setCurrentImage(currentImage+1)}>next</button> */}
+
         </div>
       )
 }
@@ -65,3 +91,6 @@ export default ImageSlider
 
 
 
+{/* 
+                        
+*/}
